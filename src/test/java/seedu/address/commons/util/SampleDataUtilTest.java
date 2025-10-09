b14e11816c6fa000246cc2874a6486d118563a99
Haffner32 <adenchan01@gmail.com>
2025-10-09 14:27:03 +0800

    edit for style check

--- src/test/java/seedu/address/commons/util/SampleDataUtilTest.java
+++ src/test/java/seedu/address/commons/util/SampleDataUtilTest.java
@@ -1,7 +1,9 @@
 package seedu.address.commons.util;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.person.Person;
 import seedu.address.model.util.SampleDataUtil;
 
@@ -15,7 +17,7 @@ public class SampleDataUtilTest {
         for (Person p : people) {
             // assuming Person#getTags() returns a Set<Tag> and Tag#tagName is accessible or via toString()
             p.getTags().forEach(t -> {
-                String v = t.toString().replace("[","").replace("]","");
+                String v = t.toString().replace("[", "").replace("]", "");
                 assertTrue(
                         v.equals("Customer") || v.equals("Supplier") || v.equals("Staff"),
                         "unexpected tag " + v
