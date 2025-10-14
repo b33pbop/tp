b024cb867de0e58c31f6b954cd1c421a08cd6d1a
Gerron <gerronleeyf02@gmail.com>
2025-10-15 00:17:30 +0800

    Add skeleton of Supplier.java

---
+++ src/main/java/seedu/address/model/person/Supplier.java
@@ -0,0 +1,68 @@
+package seedu.address.model.person;
+
+import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
+
+//import java.util.Collections;
+//import java.util.HashSet;
+//import java.util.Objects;
+import java.util.Set;
+
+//import seedu.address.commons.util.ToStringBuilder;
+import seedu.address.model.tag.Tag;
+
+/**
+ * Represents a Person in the address book.
+ * Guarantees: details are present and not null, field values are validated, immutable.
+ */
+public class Supplier extends Person{
+
+    // Identity fields
+    private final String item;
+    /**
+     * Every field must be present and not null.
+     */
+    public Supplier(Name name, Phone phone, Email email, Address address, Set<Tag> tags, String item) {
+        super(name, phone, email, address, tags);
+        requireAllNonNull(name, phone, email, address, tags);
+        this.item = item;
+    }
+
+    /**
+     * Returns true if both persons have the same name.
+     * This defines a weaker notion of equality between two persons.
+     */
+    public boolean isSameSupplier(Supplier otherPerson) {
+        if (otherPerson == this) {
+            return true;
+        }
+
+        return otherPerson != null
+                && otherPerson.equals(this);
+    }
+
+    /**
+     * Returns true if both persons have the same identity and data fields.
+     * This defines a stronger notion of equality between two persons.
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof Supplier)) {
+            return false;
+        }
+
+        Supplier otherPerson = (Supplier) other;
+        return otherPerson.item.equals(this.item);
+    }
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+
+}
