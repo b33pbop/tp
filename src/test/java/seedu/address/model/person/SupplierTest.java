389357bc39d9e347bcd03f9606ffd6e4b35b2399
Haffner32 <adenchan01@gmail.com>
2025-10-15 22:03:18 +0800

    edit for style checking once more

--- src/test/java/seedu/address/model/person/SupplierTest.java
+++ src/test/java/seedu/address/model/person/SupplierTest.java
@@ -3,11 +3,11 @@ package seedu.address.model.person;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.LocalDate;
-
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
+
 import seedu.address.model.category.Category;
 
 public class SupplierTest {
@@ -27,8 +27,8 @@ public class SupplierTest {
                 LocalDate.of(2023, 11, 2));
         test.addOrder(test1);
         test.addOrder(test2);
-        String expected = "5 of Computer (at $100.0 each) to be delivered 2023-10-02" +
-                '\n' + "10 of Computer (at $120.0 each) to be delivered 2023-11-02";
+        String expected = "5 of Computer (at $100.0 each) to be delivered 2023-10-02"
+              + '\n' + "10 of Computer (at $120.0 each) to be delivered 2023-11-02";
         assertEquals(expected, test.listOrders());
     }
 }
