b6afd6a2c2d1bdfdae15c886bbf6f8d9f6d79e3f
Gerron <gerronleeyf02@gmail.com>
2025-10-23 00:52:13 +0800

    Update deliveryDay field type

--- src/main/java/seedu/address/model/person/Order.java
+++ src/main/java/seedu/address/model/person/Order.java
@@ -10,20 +10,20 @@ public class Order {
     private String item;
     private int quantity;
     private double unitPrice;
-    private LocalDate deliveryDate;
+    private String deliveryDay;
 
     /**
      * Constructor for order
      * @param item name of item to be ordered
      * @param quantity quantity of item
      * @param unitPrice price of each item
-     * @param deliveryDate date to be delivered
+     * @param deliveryDay day to be delivered
      */
-    public Order(String item, int quantity, double unitPrice, LocalDate deliveryDate) {
+    public Order(String item, int quantity, double unitPrice, String deliveryDay) {
         this.item = item;
         this.quantity = quantity;
         this.unitPrice = unitPrice;
-        this.deliveryDate = deliveryDate;
+        this.deliveryDay = deliveryDay;
     }
 
     public String getItem() {
@@ -38,14 +38,14 @@ public class Order {
         return unitPrice;
     }
 
-    public LocalDate getDeliveryDate() {
-        return deliveryDate;
+    public String getDeliveryDay() {
+        return deliveryDay;
     }
 
     @Override
     public String toString() {
         return String.format("%s of %s (at $%s each) to be delivered %s",
-                getQuantity(), getItem(), getUnitPrice(), getDeliveryDate());
+                getQuantity(), getItem(), getUnitPrice(), getDeliveryDay());
     }
 
 
