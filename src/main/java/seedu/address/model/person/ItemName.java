b231e2cb5f3f5d4f7e9dc91bbb72d6f58a2c23ba
Gerron <gerronleeyf02@gmail.com>
2025-10-28 01:26:40 +0800

    Add ItemName class to enable checks for item name input from user

---
+++ src/main/java/seedu/address/model/person/ItemName.java
@@ -0,0 +1,69 @@
+package seedu.address.model.person;
+
+import static java.util.Objects.requireNonNull;
+import static seedu.address.commons.util.AppUtil.checkArgument;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+/**
+ * Represents an Order's item's name in the address book.
+ * Guarantees: immutable; is valid as declared in {@link #isValidItemName(String)}
+ */
+public class ItemName {
+    public static final String MESSAGE_CONSTRAINTS =
+            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "[\\p{Alnum}][\\p{Alnum} ]*";
+
+    public final String itemName;
+
+    /**
+     * Constructs a {@code Name}.
+     *
+     * @param itemName A valid name.
+     */
+    public ItemName(String itemName) {
+        requireNonNull(itemName);
+        checkArgument(isValidItemName(itemName), MESSAGE_CONSTRAINTS);
+        this.itemName = toTitleCase(itemName);
+    }
+
+    /**
+     * Returns true if a given string is a valid name.
+     */
+    public static boolean isValidItemName(String test) {
+        return test.matches(VALIDATION_REGEX);
+    }
+
+    @Override
+    public String toString() {
+        return this.itemName;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof ItemName)) {
+            return false;
+        }
+
+        ItemName otherName = (ItemName) other;
+        return this.itemName.equals(otherName.itemName);
+    }
+
+    private String toTitleCase(String input) {
+        return Arrays.stream(input.trim().split("\\s+"))
+                .map(word -> word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase())
+                .collect(Collectors.joining(" "));
+    }
+
+}
