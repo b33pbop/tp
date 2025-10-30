f3658ac02f5eb2eb951529dc10df9024634e3fca
Haffner32 <adenchan01@gmail.com>
2025-10-30 23:53:40 +0800

    add parser CustomerSummary

--- src/main/java/seedu/address/logic/parser/AddressBookParser.java
+++ src/main/java/seedu/address/logic/parser/AddressBookParser.java
@@ -12,6 +12,7 @@ import seedu.address.logic.commands.AddCommand;
 import seedu.address.logic.commands.AddOrderCommand;
 import seedu.address.logic.commands.ClearCommand;
 import seedu.address.logic.commands.Command;
+import seedu.address.logic.commands.CustomerSummaryCommand;
 import seedu.address.logic.commands.DeleteCommand;
 import seedu.address.logic.commands.EditCommand;
 import seedu.address.logic.commands.ExitCommand;
@@ -104,6 +105,8 @@ public class AddressBookParser {
         case RedeemPointsCommand.COMMAND_WORD:
             return new RedeemPointsCommandParser().parse(arguments);
 
+        case CustomerSummaryCommand.COMMAND_WORD:
+            return new CustomerSummaryCommandParser().parse(arguments);
 
         default:
             logger.finer("This user input caused a ParseException: " + userInput);
