40b8d190b93746b63dc5263ab36373783a572b9d
Gerron <gerronleeyf02@gmail.com>
2025-11-03 01:45:53 +0800

    Update Name Regex and its test cases

--- src/main/java/seedu/address/model/person/Name.java
+++ src/main/java/seedu/address/model/person/Name.java
@@ -13,13 +13,14 @@ import java.util.stream.Collectors;
 public class Name {
 
     public static final String MESSAGE_CONSTRAINTS =
-            "Names should only contain alphanumeric characters and spaces, and it should not be blank";
+            "Names should only contain alphanumeric characters and spaces, and it should not be blank. Only one pair of"
+                    + "brackets are also allowed to indicate tags. No characters after the brackets are allowed.";
 
     /*
      * The first character of the address must not be a whitespace,
      * otherwise " " (a blank string) becomes a valid input.
      */
-    public static final String VALIDATION_REGEX = "^[A-Za-z0-9 /-]+$";
+    public static final String VALIDATION_REGEX = "^[A-Za-z0-9 ]+(\\([A-Za-z0-9 ]+\\))?$";
 
     public final String fullName;
 
@@ -38,7 +39,7 @@ public class Name {
      * Returns true if a given string is a valid name.
      */
     public static boolean isValidName(String test) {
-        return test.matches(VALIDATION_REGEX);
+        return test.trim().matches(VALIDATION_REGEX);
     }
 
 
